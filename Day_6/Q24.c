998d46b09f8f09e7fe8fc9b92bfc803f8ea352c0
shekharchoudhary-71 <shekharchoudhary636@gmail.com>
2026-06-03 21:53:25 +0530

    Add program to find x^n without pow().

---
+++ Day_6/Q24.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int x, n, result = 1;
+    printf("Enter base:");
+    scanf("%d", &x);
+
+    printf("Enter power (n): ");
+    scanf("%d", &n);
+
+    for (int i = 1; i<=n; i++) {
+        result = result * x;
+    }
+
+    printf("%d^%d = %d", x, n, result);
+    return 0;
+}
