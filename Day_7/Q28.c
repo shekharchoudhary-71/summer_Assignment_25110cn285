33272c076b2ade5f00798fc6d080a7bce89e1c61
shekharchoudhary-71 <shekharchoudhary636@gmail.com>
2026-06-04 18:17:40 +0530

    Add function to reverse an integer.

---
+++ Day_7/Q28.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int rev = 0;
+
+int reverse(int n)
+{
+    if (n == 0)
+        return rev;
+
+    rev = rev * 10 + n % 10;
+    return reverse(n / 10);
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number:");
+    scanf("%d", &num);
+
+    printf("Reversed number = %d", reverse(num));
+    return 0;
+}
