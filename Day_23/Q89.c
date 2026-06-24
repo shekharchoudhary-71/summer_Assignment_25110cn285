834715e6194ffe4643056dda265b0ff59cbdd9bc
shekharchoudhary-71 <shekharchoudhary636@gmail.com>
2026-06-24 07:53:51 +0530

    Add program to find first non-repeating character.

---
+++ Day_23/Q89.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int freq[256] = {0};
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+        freq[(unsigned char)str[i]]++;
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(freq[(unsigned char)str[i]] == 1) {
+            printf("First non-repeating character: %c", str[i]);
+            return 0;
+        }
+    }
+
+    printf("No non-repeating character found.");
+    return 0;
+}
