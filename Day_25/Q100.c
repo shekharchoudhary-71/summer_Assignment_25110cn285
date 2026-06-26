a12831fea5346e7b26c2e21f9dfb5b5da35cdc57
shekharchoudhary-71 <shekharchoudhary636@gmail.com>
2026-06-26 21:43:12 +0530

    Add word length sorting program in Q100.c
    This program sorts a list of words based on their length and prints them.

---
+++ Day_25/Q100.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of words: ");
+    scanf("%d",&n);
+
+    char word[n][50], temp[50];
+
+    for(int i=0;i<n;i++)
+        scanf("%s",word[i]);
+
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(strlen(word[i])>strlen(word[j])){
+                strcpy(temp,word[i]);
+                strcpy(word[i],word[j]);
+                strcpy(word[j],temp);
+            }
+        }
+    }
+
+    printf("Words sorted by length:\n");
+
+    for(int i=0;i<n;i++)
+        printf("%s\n",word[i]);
+
+    return 0;
+}
